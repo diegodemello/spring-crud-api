7b95caae19713030fa769a65b9c8d58c55dba891
Diego de Mello <diegodemello027@gmail.com>
2025-05-21 19:18:36 -0300

    feat: Cria as rotas do controller de missões com mock.

--- src/main/java/com/diego/CadastroDeNinjas/Missions/MissionController.java
+++ src/main/java/com/diego/CadastroDeNinjas/Missions/MissionController.java
@@ -1,9 +1,35 @@
 package com.diego.CadastroDeNinjas.Missions;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/mission")
 public class MissionController {
+
+    @PostMapping("add")
+    public void addMission(MissionModel missionModel){
+
+    }
+
+    @GetMapping("/list")
+    public List<MissionModel> listMissions(){
+        return listMissions();
+    }
+
+    @GetMapping("/id")
+    public MissionModel missionModel(Long id){
+        return missionModel(id);
+    }
+
+    @PutMapping("/changeID")
+    public void changeMission(MissionModel missionModel){
+
+    }
+
+    @DeleteMapping("/id")
+    public void deleteMission(MissionModel missionModel){
+
+    }
 }
