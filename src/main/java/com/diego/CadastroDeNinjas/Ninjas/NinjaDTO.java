554ea551b3fe44b2f7978a7068e4ad33aec37ed9
Diego de Mello <diegodemello027@gmail.com>
2025-05-23 14:21:39 -0300

    chore: Inclui a versão do DTO pela entidade e mapeia manualmente no NinjaMapper.

--- src/main/java/com/diego/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/com/diego/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,18 @@
 package com.diego.CadastroDeNinjas.Ninjas;
 
+import com.diego.CadastroDeNinjas.Missions.MissionModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private int age;
+    private String rank;
+    private MissionModel mission;
 }
