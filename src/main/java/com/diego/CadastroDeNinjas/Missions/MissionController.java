ca1f46f05f8dd63d3e16c76fa4ffaa1c26961d22
Diego de Mello <diegodemello027@gmail.com>
2025-05-26 19:28:40 -0300

    doc: Adiciona annotations do Swagger na aplicação para documentar as rotas no controller de missões.

--- src/main/java/com/diego/CadastroDeNinjas/Missions/MissionController.java
+++ src/main/java/com/diego/CadastroDeNinjas/Missions/MissionController.java
@@ -1,6 +1,9 @@
 package com.diego.CadastroDeNinjas.Missions;
 
 import com.diego.CadastroDeNinjas.Ninjas.NinjaModel;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,17 +19,28 @@ public class MissionController {
     }
 
     @PostMapping("/add")
+    @Operation(summary = "Cria uma nova missão", description = "Rota cria uma nova missão e insere no banco de dados.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Missão criada com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Erro na criação da missão.")
+    })
     public ResponseEntity<String> addMission(@RequestBody MissionDTO missionDTO){
         MissionDTO mission = missionService.addMission(missionDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body("Missão de " + missionDTO.getName() + " criada com sucesso!");
     }
 
     @GetMapping("/list")
+    @Operation(summary = "Lista todas as missões", description = "Rota lista todas as missões cadastradas.")
     public ResponseEntity<List<MissionDTO>> listMissions(){
         return ResponseEntity.ok().body(missionService.listMissions());
     }
 
     @GetMapping("/list/{id}")
+    @Operation(summary = "Lista missão por ID", description = "Rota lista uma missão através do seu ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Missão encontrado com sucesso."),
+            @ApiResponse(responseCode = "404", description = "Missão não encontrado.")
+    })
     public ResponseEntity<?> listMissionById(@PathVariable Long id){
         if(missionService.listMissionById(id) != null){
             MissionDTO mission = missionService.listMissionById(id);
@@ -37,6 +51,11 @@ public class MissionController {
     }
 
     @PutMapping("/change/{id}")
+    @Operation(summary = "Altera a missão por ID", description = "Rota altera a missão e insere no banco de dados.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Missão alterada com sucesso."),
+            @ApiResponse(responseCode = "404", description = "Missão não encontrada, impossível alterar.")
+    })
     public ResponseEntity<String> changeMission(@PathVariable Long id, @RequestBody MissionDTO missionDTO){
         if(missionService.listMissionById(id) != null){
             missionService.changeMission(id, missionDTO);
@@ -47,6 +66,7 @@ public class MissionController {
     }
 
     @DeleteMapping("/delete/{id}")
+    @Operation(summary = "Deleta a missão através do ID", description = "Rota deleta a missão do banco de dados.")
     public ResponseEntity<String> deleteMission(@PathVariable Long id){
         if(missionService.listMissionById(id) != null){
             missionService.deleteMission(id);
